30091906b58ceff1c0e205f92f9269a2d9149539
Carlson Lam <4546682+ksclam@users.noreply.github.com>
2021-02-03 00:14:30 -0500

    Added steps for setting IP address after factory default

--- CS463FactoryDefault.py
+++ CS463FactoryDefault.py
@@ -1,5 +1,7 @@
 import sys
+import time
 import serial
+import requests
 from datetime import datetime
 from configparser import ConfigParser
 
@@ -7,11 +9,16 @@ serRead = ""
 response = ""
 timer = None
 backupFileName = ""
+hostname = ""
+sessionId = ""
 config = ConfigParser()
 
 try:
     config.read("config.ini")
     serPort = config.get("Serial", "Port")
+    defaultIP = config.get("Network", "DefaultIP")
+    defaultGateway = config.get("Network", "DefaultGateway")
+    defaultMask = config.get("Network", "DefaultMask")
     print("CSL Factory Default Tool")
     print("Serial Port: " + serPort)
     ser = serial.Serial(serPort, 115200, timeout=0.1)  # open serial port
@@ -22,6 +29,7 @@ except:
 def uboot_command(command, timeout):
     global response
     global serRead
+    global timer
 
     ser.write(command + b"\n")
     timer = datetime.now()
@@ -40,8 +48,9 @@ def uboot_command(command, timeout):
 def read_serial_until_string_pattern(StringPattern, timeout):
     global serRead
     global response
+    global timer
 
-    timer=datetime.now()
+    timer = datetime.now()
     while True:
         try:
             serRead = ser.read().decode("UTF-8", "replace")
@@ -62,13 +71,65 @@ def read_serial_until_string_pattern(StringPattern, timeout):
             sys.exit()
 
 
+def send_serial_command(command, resp, timeout):
+    global response
+    response = ""
+    ser.write(command)
+    if not read_serial_until_string_pattern(resp, timeout):
+        # timed out
+        ser.close()
+        print(">> Error sending serial commands '{}'.  Program abort".format(command))
+        sys.exit()
+
+
+def http_command_login(ipaddr):
+    global sessionId
+
+    p = (("command", "login"), ("username", "root"), ("password", "csl"))
+    r = requests.get("http://" + ipaddr + "/API", params=p, timeout=2.5)
+    if r.status_code == 200:
+        content = r.content.decode("UTF-8")
+        if content.find("session_id=") >= 0:
+            sessionId = content[content.find("session_id=") + 11:content.find("session_id=") + 11 + 8]
+            return True
+    return False
+
+
+def http_command_forcelogout(ipaddr):
+    p = (("command", "forceLogout"), ("username", "root"), ("password", "csl"))
+    r = requests.get("http://" + ipaddr + "/API", params=p, timeout=2.5)
+    if r.status_code == 200:
+        if r.content.decode("UTF-8").find("OK") >= 0:
+            return True
+    return False
+
+
+def http_command_setNetworkConfig(ipaddr, mask, defaultGw):
+    global sessionId
+
+    p = (("session_id", sessionId),
+         ("command", "setNetworkConfig"),
+         ("type", "ethernet"),
+         ("dhcpmode", "0"),
+         ("ip", ipaddr),
+         ("mask", mask),
+         ("gateway", defaultGw),
+         ("dns_server1", "8.8.8.8"),
+         ("dns_server2", defaultGw))
+
+    r = requests.get("http://" + ipaddr + "/API", params=p, timeout=2.5)
+    if r.status_code == 200:
+        if r.content.decode("UTF-8").find("OK:") >= 0:
+            return True
+    return False
+
 if ser.isOpen():
     ser.flush()
     print("(1) Power off your reader ")
     print("(2) Connect the serial cable from you computer to the debug serial port on the device.")
     print("(3) Power on the reader and wait...")
-    response=""
-    serRead=""
+    response = ""
+    serRead = ""
 
     if read_serial_until_string_pattern("Hit any key to stop autoboot:", 300):
         # send space character to stop reader at bootloader mode
@@ -94,122 +155,100 @@ if ser.isOpen():
         print(">> Response timed out.  Program abort")
         sys.exit()
 
-    response = ""
-    ser.write(b"boot\n")
-    if not read_serial_until_string_pattern("imx6dlsabresd login: ", 10):
-        # timed out
-        ser.close()
-        print(">> Error entering recovery partition mode.  Program abort")
-        sys.exit()
-
-    response = ""
-    ser.write(b"root\n")
-    if not read_serial_until_string_pattern("root@imx6dlsabresd:~# ", 5):
-        # timed out
-        ser.close()
-        print(">> Error logging into partition mode.  Program abort")
-        sys.exit()
-
-    response = ""
-    ser.write(b"mkdir -p backuprootfs\n")
-    if not read_serial_until_string_pattern("root@imx6dlsabresd:~# ", 5):
-        # timed out
-        ser.close()
-        print(">> Error creating /home/root/backuprootfs.  Program abort")
-        sys.exit()
-
-    response = ""
-    ser.write(b"mount /dev/mmcblk2p6 ~/backuprootfs/\n")
-    if not (read_serial_until_string_pattern("root@imx6dlsabresd:~# ", 5) and
-            ("mounted filesystem" in response)):
-        # timed out
+    send_serial_command(b"boot\n", "imx6dlsabresd login: ", 10)
+    send_serial_command(b"root\n", "root@imx6dlsabresd:~# ", 5)
+    send_serial_command(b"mkdir -p backuprootfs\n", "root@imx6dlsabresd:~# ", 5)
+    send_serial_command(b"mount /dev/mmcblk2p6 ~/backuprootfs/\n", "root@imx6dlsabresd:~# ", 5)
+    if not "mounted filesystem" in response:
         ser.close()
         print(">> Unable to mount /dev/mmcblk2p6.  Program abort")
         sys.exit()
-
-    response = ""
-    ser.write(b"ls ~/backuprootfs/ | grep FactoryDefault.tar.gz\n")
-    if not read_serial_until_string_pattern("FactoryDefault.tar.gz\r\nroot@imx6dlsabresd:~# ", 5):
-        # timed out
-        ser.close()
-        print(">> Unable to found recovery image file.  Program abort")
-        sys.exit()
+    send_serial_command(b"ls ~/backuprootfs/ | grep FactoryDefault.tar.gz\n", "FactoryDefault.tar.gz\r\nroot@imx6dlsabresd:~# ", 5)
 
     # save the file name
-    startIndex=0
-    endIndex=0
-    startIndex=response.find("rootfs",
+    startIndex = 0
+    endIndex = 0
+    startIndex = response.find("rootfs",
                              len("ls ~/backuprootfs/ | grep FactoryDefault.tar.gz\n"))
-    endIndex=response.find("root@imx6dlsabresd:~#") - 2
+    endIndex = response.find("root@imx6dlsabresd:~#") - 2
     backupFileName = response[startIndex:endIndex]
 
-
-    response = ""
-    ser.write(b"mkdir -p originalrootfs\n")
-    if not read_serial_until_string_pattern("root@imx6dlsabresd:~# ", 5):
-        # timed out
-        ser.close()
-        print(">> Error creating /home/root/originalrootfs.  Program abort")
-        sys.exit()
-
-    response = ""
-    ser.write(b"mount /dev/mmcblk2p2 ~/originalrootfs/\n")
-    if not (read_serial_until_string_pattern("root@imx6dlsabresd:~# ", 5) and
-            ("mounted filesystem" in response)):
-        # timed out
+    send_serial_command(b"mkdir -p originalrootfs\n", "root@imx6dlsabresd:~# ", 5)
+    send_serial_command(b"mount /dev/mmcblk2p2 ~/originalrootfs/\n", "root@imx6dlsabresd:~# ", 5)
+    if not "mounted filesystem" in response:
         ser.close()
         print(">> Unable to mount /dev/mmcblk2p2.  Program abort")
         sys.exit()
+    send_serial_command(b"ls ~/originalrootfs/ | grep opt\n", "opt\r\nroot@imx6dlsabresd:~#", 5)
+    send_serial_command(b"cd ~/originalrootfs\n", "root@imx6dlsabresd:~/originalrootfs# ", 5)
+    send_serial_command(b"rm * -rf\n", "root@imx6dlsabresd:~/originalrootfs# ", 300)
+    send_serial_command(b"export EXTRACT_UNSAFE_SYMLINKS=1\n", "root@imx6dlsabresd:~/originalrootfs# ", 5)
+    send_serial_command(("tar xvzf ~/backuprootfs/" + backupFileName + "\n").encode('utf-8'), "root@imx6dlsabresd:~/originalrootfs# ", 1200)
+    send_serial_command(b"ls /etc/systemd/system | grep default.target\n", "default.target\r\nroot@imx6dlsabresd:~/originalrootfs# ", 5)
+    send_serial_command(b"reboot\n", "imx6dlsabresd login: ", 60)
+    send_serial_command(b"root\n", "Password: ", 5)
+    send_serial_command(b"csl\n", "root@imx6dlsabresd:~# ", 5)
+    send_serial_command(b"/opt/hostnameUnique_set\n", "root@imx6dlsabresd:~# ", 5)
 
-    response = ""
-    ser.write(b"ls ~/originalrootfs/ | grep opt\n")
-    if not read_serial_until_string_pattern("opt\r\nroot@imx6dlsabresd:~#", 5):
+    # extract hostname
+    startIndex = response.find("/opt/hostnameUnique_set") + len("/opt/hostnameUnique_set") + 2;
+    endIndex = response.find("root@imx6dlsabresd:~#") - 2;
+    if startIndex < 0 or endIndex < 0 or (startIndex > endIndex):
         # timed out
         ser.close()
-        print(">> Unable to find original rootfs in mounted folder.  Program abort")
+        print(">> Error setting hostname  Program abort")
+        sys.exit()
+    hostname = response[startIndex:endIndex]
+
+    # hostname = "csl-7b8401f6"
+    send_serial_command(b"reboot\n", hostname + " login: ", 60)
+
+    if defaultIP == "":
+        print("\n>> Factory default completed.  Program exit.")
         sys.exit()
 
-    response = ""
-    ser.write(b"cd ~/originalrootfs\n")
-    if not read_serial_until_string_pattern("root@imx6dlsabresd:~/originalrootfs# ", 5):
+    send_serial_command(b"root\n", "Password: ", 5)
+    send_serial_command(b"csl\n", "root@" + hostname + ":~# ", 5)
+
+    print(">> Set new IP address")
+    send_serial_command(b"ps aux | grep connman\n", "root@csl-7b8401f6:~# ", 5)
+
+    psOutput = response.split("\r\n");
+    ps = ""
+    for line in psOutput:
+        if line.find("/usr/sbin/connmand -n") >= 0:
+            ps = line.split()[1]
+
+    if ps == "":
         # timed out
         ser.close()
-        print(">> Unable to enter original rootfs.  Program abort")
+        print(">> Unable to reboot device.  Program abort")
         sys.exit()
 
-    response = ""
-    ser.write(b"rm * -rf\n")
-    if not read_serial_until_string_pattern("root@imx6dlsabresd:~/originalrootfs# ", 300):
-        # timed out
+    send_serial_command(("kill " + ps + "\n").encode("utf-8"), "root@" + hostname + ":~# ", 5)
+    send_serial_command(b"ifconfig eth0 up\n", "root@" + hostname + ":~# ", 5)
+    send_serial_command(("ifconfig eth0 " + defaultIP + "\n").encode("utf-8"), "root@" + hostname + ":~# ", 5)
+
+    for i in range(120, -1, -1):
+        time.sleep(1)
+        print("\r>> Wait for {0:d} seconds".format(i), end="")
+
+    print(">> Web interface is now accessible through new IP.  Complete network configurations through HTTP commands")
+
+    # login
+    if not http_command_login(defaultIP):
         ser.close()
-        print(">> Unable to delete original rootfs.  Program abort")
+        print(">> Unable to login reader through HTTP.  Program abort")
         sys.exit()
 
-    response = ""
-    ser.write(b"export EXTRACT_UNSAFE_SYMLINKS=1\n")
-    if not read_serial_until_string_pattern("root@imx6dlsabresd:~/originalrootfs# ", 300):
-        # timed out
+    # set network config
+    if not http_command_setNetworkConfig(defaultIP, defaultMask, defaultGateway):
         ser.close()
-        print(">> Unable to configure extraction process.  Program abort")
+        print(">> Unable to set network config through HTTP.  Program abort")
         sys.exit()
 
-    response = ""
-    ser.write(("tar xvzf ~/backuprootfs/" + backupFileName + "\n").encode('utf-8'))
-    if not read_serial_until_string_pattern("root@imx6dlsabresd:~/originalrootfs# ", 1200):
-        # timed out
-        ser.close()
-        print(">> Unable to restore rootfs.  Program abort")
-        sys.exit()
-
-    response = ""
-    ser.write(b"ls /etc/systemd/system | grep default.target\n")
-    if not read_serial_until_string_pattern("default.target\r\nroot@imx6dlsabresd:~/originalrootfs# ", 5):
-        # timed out
-        ser.close()
-        print(">> Unable to find original rootfs in mounted folder.  Program abort")
-        sys.exit()
-
-    print("\n>> Factory default completed.  Please power cycle the reader")
+    send_serial_command(b"reboot\n", hostname + " login: ", 60)
+    print("\n>> Factory default completed.  Program exit.")
 
 else:
     print("Serial port cannot be opened")
--- README.md
+++ README.md
@@ -14,6 +14,24 @@ Command-line tool for putting CSL fixed readers back to factory default mode thr
 2. Connect the debug serial port on the reader to your computer
 3. Download the package and extract the zip file under the *dist* folder
 3. Edit the *config.ini* file and enter your serial port information.  For Windows it would be COM{port number}.  For Mac it would be the device under /dev/tty.usbserial-{deviceId}
-4. Run the file CS463FactoryDefault.exe in the folder
-5. Power up the reader and it will boot into recovery mode.  The entire file system will be refreshed to default.  The process would usually take around 15 minutes
+	
+	```
+	[Serial]
+	Port = COM6
+	```
+	
+4. The program can also set the IP address of the reader after resetting the system.  For doing that, you will need to update the IP information in *config.ini*:
+	
+	```
+	[Network]
+	DefaultIP = 192.168.1.100
+	DefaultMask = 255.255.255.0
+	DefaultGateway = 192.168.1.1
+	```
+	After that, please also connect your computer to the reader via the same network being configure.  For the example above, the IP address of the network interface connected to the reader should be in the same network 192.168.1.XXX 
+	
+	If network information is not entered in *config.ini*, IP address will not be set after factory default and it will be stay in the default value 192.168.25.160.
+	
+5. Run the file CS463FactoryDefault.exe in the folder
+6. Power up the reader and it will boot into recovery mode.  The entire file system will be refreshed to default.  The process would usually take around 15 minutes
 
